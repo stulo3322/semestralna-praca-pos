a3ecc9db55d948d748cff2fa78d7a3f404b08fff
stulo3322 <martin.stulrajter1@gmail.com>
2020-12-30 16:10:55 +0100

    prva

---
+++ cmake-build-debug/CMakeFiles/piskvorky.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/piskvorky.dir/main.c.o"
+  "CMakeFiles/piskvorky.dir/client.c.o"
+  "CMakeFiles/piskvorky.dir/server.c.o"
+  "piskvorky.pdb"
+  "piskvorky"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/piskvorky.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
