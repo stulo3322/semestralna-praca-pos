879f6ac0bf1ee44e20892dda8c01c90aaf6ee9ae
stulo3322 <martin.stulrajter1@gmail.com>
2021-01-05 16:20:33 +0100

    ver

--- client.c
+++ client.c
@@ -8,19 +8,9 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    int sockfd, n;
+    int sockfd;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-
-    char buffer[256];
-    int tah = 0;
-
-    int riadok = 0;
-    int stlpec = 0;
-    char hraciaPlocha[3][3] = {{'1', '2', '3'},
-                               {'4', '5', '6'},
-                               {'7', '8', '9'}};
-
 
     if (argc < 3) {
         fprintf(stderr, "usage %s hostname port\n", argv[0]);
@@ -50,76 +40,128 @@ int main(int argc, char *argv[]) {
     }
 
 
+    int znova = 0;
+    do {
+        /*
+        int hrac = 0;
+        int vitaz = 0;
+        int spravne = 0;
+        char policko = ' ';
+        int opacne = 0;
+        int tah = 0;
+        int riadok = 0;
+        int stlpec = 0;
+        char hraciaPlocha[3][3] = {{'1', '2', '3'},
+                                   {'4', '5', '6'},
+                                   {'7', '8', '9'}};
+        for (int i = 0; i < 9 && vitaz == 0; ++i) {
+            printf("zaciatok for\n");
 
-    int vitaz = 0;
-    int spravne = 0;
-    char policko = ' ';
-    int opacne = 0;
-
-
-    for (int i = 0; i < 9 && vitaz == 0; ++i) {
-        if(i == 0) {
-            recv(sockfd, &tah, 200, 0);
-            if(tah == 5) {
-                opacne = 1;
+            if(i == 0) {
+                recv(sockfd, &tah, 200, 0);
+                printf("opacne %d\n",tah);
+                if(tah == 5) {
+                    opacne = 1;
+                }
+                for (int o = 0; o < 3; o++) {
+                    printf(" %c | %c | %c\n", hraciaPlocha[o][0], hraciaPlocha[o][1], hraciaPlocha[o][2]);
+                    if (o < 2) {
+                        printf("---+---+---\n");
+                    }
+                }
             }
-        }
 
-        if(opacne == 0) {
-
-        }
-        printf("Cakaj kym protihrac vyberie policko\n");
-        recv(sockfd, &tah, 200, 0);
-        printf("Protihrac vybral policko %d\n", tah);
-
-        riadok = --tah / 3;
-        stlpec = tah % 3;
-        hraciaPlocha[riadok][stlpec] = 'X';
-
-        for (int o = 0; o < 3; o++) {
-            printf(" %c | %c | %c\n", hraciaPlocha[o][0], hraciaPlocha[o][1], hraciaPlocha[o][2]);
-            if (o < 2) {
-                printf("---+---+---\n");
+            if(opacne == 1) {
+                hrac = i%2;
+            } else {
+                hrac = i%2 + 1;
             }
-
-            printf("\n\n");
-
-            printf("Zadaj cislo stvorca: ");
 
             do {
-                scanf("%d", &tah);
-                if (tah > 0 && tah < 10) {
+                spravne = 0;
+                if(hrac == 1) {
+                    printf("Cakaj kym protihrac vyberie policko\n");
+                    recv(sockfd, &tah, 200, 0);
+                    printf("Protihrac vybral policko %d\n", tah);
+
                     riadok = --tah / 3;
                     stlpec = tah % 3;
-                    policko = hraciaPlocha[riadok][stlpec];
+                    hraciaPlocha[riadok][stlpec] = opacne == 1 ? 'O' : 'X';
 
-                    if (policko != 'O' || policko != 'X') {
-                        spravne = 1;
-                    } else {
-                        printf("Zadali ste uz obsadene policko. \n");
+                    for (int o = 0; o < 3; o++) {
+                        printf(" %c | %c | %c\n", hraciaPlocha[o][0], hraciaPlocha[o][1], hraciaPlocha[o][2]);
+                        if (o < 2) {
+                            printf("---+---+---\n");
+                        }
                     }
+                    printf("\n\n");
+                    spravne = 1;
+
                 } else {
-                    printf("Zadali ste nespravne policko. \n");
+                    printf("Zadaj cislo stvorca: ");
+
+                    do {
+                        scanf("%d", &tah);
+                        printf("nacital som %d\n",tah);
+                        if (tah > 0 && tah < 10) {
+                            riadok = --tah/3;
+                            stlpec = tah%3;
+                            tah++;
+                            policko = hraciaPlocha[riadok][stlpec];
+                            printf("po vypoctoch je tah %d\n",tah);
+
+                            if (policko != 'O' || policko != 'X') {
+                                spravne = 1;
+                            } else {
+                                printf("Zadali ste uz obsadene policko. \n");
+                            }
+                        } else {
+                            printf("Zadali ste nespravne policko. \n");
+                        }
+
+                    } while (spravne != 1);
+
+                    send(sockfd, &tah, sizeof(tah), 0);
+                    printf("odoslal som %d\n",tah);
+                    hraciaPlocha[riadok][stlpec] = opacne == 1 ? 'X' : 'O';
+
+                    for (int o = 0; o < 3; o++) {
+                        printf(" %c | %c | %c\n", hraciaPlocha[o][0], hraciaPlocha[o][1], hraciaPlocha[o][2]);
+                        if (o < 2) {
+                            printf("---+---+---\n");
+                        }
+                    }
+
+                    printf("\n\n");
                 }
-
-            } while (spravne != 1);
-
-
-            send(sockfd, &tah, sizeof(tah), 0);
-            hraciaPlocha[riadok][stlpec] = 'O';
-
-            for (int o = 0; o < 3; o++) {
-                printf(" %c | %c | %c\n", hraciaPlocha[o][0], hraciaPlocha[o][1], hraciaPlocha[o][2]);
-                if (o < 2) {
-                    printf("---+---+---\n");
-                }
-            }
-
-            printf("\n\n");
+            } while(spravne != 1);
+            printf("Cakam na vitaza\n");
+            recv(sockfd, &vitaz, 200, 0);
+            printf("Prijal som vitaza %d\n",vitaz);
         }
 
-        close(sockfd);
+        if(vitaz == 0) {
+            printf("Remiza\n");
+        } else if (vitaz == 1) {
+            printf("Prehral si\n");
+        } else {
+            printf("Vyhral si\n");
+        }
+        */
+        printf("Chces hrat znova? (1 - ano, 2 - nie)\n");
+        scanf("%d", &znova);
+        if(znova == 1) {
+            send(sockfd, &znova, sizeof(znova), 0);
+            printf("Cakanie na potvrdenie od protihraca\n");
+            if(recv(sockfd, &znova, 200, 0) == 0) {
+                printf("Protihrac zamietol ponuku o hranie znova\n");
+            }
+        } else {
+            send(sockfd, &znova, sizeof(znova), 0);
+        }
+    } while(znova == 1);
 
-        return 0;
-    }
+
+    close(sockfd);
+    return 0;
 }
--- server.c
+++ server.c
@@ -28,88 +28,149 @@ typedef struct {
 
 
 void* vlaknoHry(void*args) {
-
     dataHra * dat = (dataHra *)args;
     pthread_mutex_lock(dat->mutex);
-    if(dat->mainData->pocetVytvorenychHier > 1) {
-        pthread_cond_wait(dat->mozeSaHrat,dat->mutex);
-    }
     int opacne = 0;
+    int znova = 0;
     int n = 0;
-    char hraciaPlocha[3][3] = {{'1', '2', '3'},
-                               {'4', '5', '6'},
-                               {'7', '8', '9'}};
 
-    for (int i = 0; i < 9 && dat->vitaz == 0; i++) {
-        printf("\n\n");
-        for (int o = 0; o < 3; o++) {
+    do {
+        /*
+        dat->vitaz = 0;
+        char hraciaPlocha[3][3] = {{'1', '2', '3'},
+                                   {'4', '5', '6'},
+                                   {'7', '8', '9'}};
+        for (int i = 0; i < 9 && dat->vitaz == 0; i++) {
+            printf("zaciatok for\n");
+            printf("\n\n");
+            for (int o = 0; o < 3; o++) {
                 printf(" %c | %c | %c\n", hraciaPlocha[o][0], hraciaPlocha[o][1], hraciaPlocha[o][2]);
                 printf("---+---+---\n");
-        }
-
-        if(i == 0) {
-            dat->hrac = rand() < 0.5 ? 1 : 0;
-            if (dat->hrac == 1) {
-                opacne = 1;
-                dat->tah = 5;
             }
-            send(dat->sockfd,&dat->tah,sizeof(dat->tah),0);
 
-        }
-        if(opacne == 1) {
-            dat->hrac = i%2;
-        } else {
-            dat->hrac = i%2 + 1;
-        }
-
-
-        do {
-            if(dat->hrac == 1) {
-                printf("Zadaj cislo stvorca: \n");
-                scanf("%d", &dat->tah);
-                send(dat->sockfd,&dat->tah,sizeof(dat->tah),0);
-
-            } else {
-                printf("Cakaj kym protihrac vyberie policko.\n");
-                n = recv(dat->sockfd,&dat->tah,200,0);
-                if(n<1) {
-                    printf("Nepodarilo sa ziskat tah");
+            if (i == 0) {
+                float nahodne =  rand() % 101;
+                dat->hrac = nahodne < 50 ? 1 : 0;
+                printf("hodnota rand pre hraca %f\n",nahodne);
+                if (dat->hrac == 1) {
+                    opacne = 1;
+                    dat->tah = 5;
                 }
-                printf("Protihrac vybral policko %d\n",dat->tah);
+                sleep(1);
+                send(dat->sockfd, &dat->tah, sizeof(dat->tah), 0);
+                printf("odoslanie opacne cez tah\n");
+                printf("hodnota tahu %d\n",dat->tah);
+
+            }
+            if (opacne == 1) {
+                if(i%2==0) {
+                    dat->hrac =  2;
+                } else {
+                    dat->hrac =  1;
+                }
+                printf("hrame opacne\n");
+            } else {
+                if(i%2==0) {
+                    dat->hrac =  1;
+                } else {
+                    dat->hrac =  2;
+                }
+                printf("hrame normalne\n");
             }
 
-            dat->riadok = --dat->tah/3;
-            dat->stlpec = dat->tah%3;
-        } while(dat->tah<0 || dat->tah > 9 || hraciaPlocha[dat->riadok][dat->stlpec]>'9');
+            int jebetomu = 0;
+            do {
+                jebetomu++;
+                if (dat->hrac == 1) {
+                    printf("Zadaj cislo stvorca: \n");
+                    scanf("%d", &dat->tah);
+                    send(dat->sockfd, &dat->tah, sizeof(dat->tah), 0);
 
-        hraciaPlocha[dat->riadok][dat->stlpec] = (dat->hrac == 1) ? 'X' : 'O';
+                } else {
+                    printf("Cakaj kym protihrac vyberie policko.\n");
+                    n = recv(dat->sockfd, &dat->tah, 200, 0);
+                    if (n < 1) {
+                        printf("Nepodarilo sa ziskat tah");
+                    }
+                    printf("Protihrac vybral policko %d\n", dat->tah);
+                }
 
-        if((hraciaPlocha[0][0] == hraciaPlocha[1][1] && hraciaPlocha[0][0] == hraciaPlocha[2][2]) ||
-           (hraciaPlocha[0][2] == hraciaPlocha[1][1] && hraciaPlocha[0][2] == hraciaPlocha[2][0]))
-            dat->vitaz = dat->hrac;
-        else
-            for(int j = 0; j <= 2; j++)
-                if((hraciaPlocha[j][0] == hraciaPlocha[j][1] && hraciaPlocha[j][0] == hraciaPlocha[j][2])||
-                   (hraciaPlocha[0][j] == hraciaPlocha[1][j] && hraciaPlocha[0][j] == hraciaPlocha[2][j]))
-                    dat->vitaz = dat->hrac;
-    }
+                dat->riadok = --dat->tah / 3;
+                dat->stlpec = dat->tah % 3;
+                dat->tah++;
 
-    printf("\n\n");
-    for (int o = 0; o < 3; o++) {
-        printf(" %c | %c | %c\n", hraciaPlocha[o][0], hraciaPlocha[o][1], hraciaPlocha[o][2]);
-        printf("---+---+---\n");
-    }
+            } while ((dat->tah < 1 || dat->tah > 9 || hraciaPlocha[dat->riadok][dat->stlpec] > '9') && jebetomu < 5);
 
-    if(dat->vitaz == 0) {
-        printf("Remiza\n");
-    } else if (dat->vitaz == 1) {
-        printf("Vyhral si\n");
-    } else {
-        printf("Prehral si\n");
-    }
+            hraciaPlocha[dat->riadok][dat->stlpec] = (dat->hrac == (opacne == 1 ? 2:1)) ? 'X' : 'O';
+
+            if ((hraciaPlocha[0][0] == hraciaPlocha[1][1] && hraciaPlocha[0][0] == hraciaPlocha[2][2]) ||
+                (hraciaPlocha[0][2] == hraciaPlocha[1][1] && hraciaPlocha[0][2] == hraciaPlocha[2][0])) {
+                dat->vitaz = dat->hrac;
+            } else {
+                for (int j = 0; j <= 2; j++) {
+                    if ((hraciaPlocha[j][0] == hraciaPlocha[j][1] && hraciaPlocha[j][0] == hraciaPlocha[j][2]) ||
+                        (hraciaPlocha[0][j] == hraciaPlocha[1][j] && hraciaPlocha[0][j] == hraciaPlocha[2][j])) {
+                        dat->vitaz = dat->hrac;
+                    }
+                }
+            }
+            printf("skontrolovanie vitaza\n");
+            sleep(1);
+            send(dat->sockfd, &dat->vitaz, sizeof(dat->vitaz), 0);
+            printf("Odoslal som vitaza %d\n",dat->vitaz);
+        }
+
+        printf("\n\n");
+        for (int o = 0; o < 3; o++) {
+            printf(" %c | %c | %c\n", hraciaPlocha[o][0], hraciaPlocha[o][1], hraciaPlocha[o][2]);
+            printf("---+---+---\n");
+        }
+
+        if(dat->vitaz == 0) {
+            printf("Remiza\n");
+        } else if (dat->vitaz == 1) {
+            printf("Vyhral si\n");
+        } else {
+            printf("Prehral si\n");
+        }
+        */
+        printf("Chces hrat znova? (1 - ano, 2 - nie)\n");
+        scanf("%d", &znova);
+        if(znova == 1) {
+            send(dat->sockfd, &znova, sizeof(znova), 0);
+            printf("Cakanie na potvrdenie od protihraca\n");
+            if(recv(dat->sockfd, &znova, 200, 0) == 0) {
+                znova = 2;
+                printf("Protihrac zamietol ponuku o hranie znova\n");
+            }
+        } else {
+            send(dat->sockfd, &znova, sizeof(znova), 0);
+        }
+        printf("Koniec DO WHILE\n");
+    } while(znova == 1);
+    printf("po DO WHILE\n");
+
     dat->mainData->pocetVytvorenychHier--;
     pthread_cond_broadcast(dat->mozeSaHrat);
     pthread_mutex_unlock(dat->mutex);
+}
+
+void* vlaknoKlient(void* args) {
+    dataHra * dat = (dataHra *)args;
+    if(dat->mainData->pocetVytvorenychHier > 1) {
+        pthread_cond_wait(dat->mozeSaHrat,dat->mutex);
+        printf("moze sa hrat\n");
+    }
+    pthread_t hra;
+    if (pthread_create(&hra,NULL,&vlaknoHry,&dat) != 0) {
+        perror("Nepodarilo sa vytvorit vlakno\n");
+        return 1;
+    } else {
+        dat->mainData->pocetVytvorenychHier++;
+        printf("pocetVytvorenychHier %d\n",dat->mainData->pocetVytvorenychHier++);
+        pthread_join(hra,NULL);
+        printf("vlakno\n");
+    }
 }
 
 int main(int argc, char *argv[])
@@ -158,7 +219,9 @@ int main(int argc, char *argv[])
     int i = 0;
     while(1) {
         cli_len = sizeof(cli_addr);
+        printf("Cakam na pripojenie protihraca... \n");
         newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &cli_len);
+        printf("pripojenie\n");
         if (newsockfd < 0)
         {
             perror("ERROR on accept");
@@ -166,15 +229,18 @@ int main(int argc, char *argv[])
         }
 
         dataHra data = {&mutex,&hraPrebieha,&mozeSaHrat,0,
-                        0,0,0,0,newsockfd};
+                        0,0,0,0,newsockfd,&mainData1};
+        printf("data\n");
 
-        pthread_t id;
-        if (pthread_create(&id,NULL,&vlaknoHry,&data) != 0) {
-            perror("Nepodarilo sa vytvorit vlakno");
+        pthread_t klient;
+        if (pthread_create(&klient,NULL,&vlaknoKlient,&data) != 0) {
+            perror("Nepodarilo sa vytvorit vlakno\n");
             return 1;
         } else {
             mainData1.pocetVytvorenychHier++;
-            pthread_join(id,NULL);
+            printf("pocetVytvorenychHier %d\n",mainData1.pocetVytvorenychHier);
+            pthread_join(klient,NULL);
+            printf("vlakno\n");
         }
     }
 
